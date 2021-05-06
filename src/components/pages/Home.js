ec3e3322767ba69b0b6b9c341d038dd66193c19c
Craig Clayton <55770352+BullDogPatch@users.noreply.github.com>
2021-05-06 13:31:06 +0100

    added classes to Link

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -6,9 +6,13 @@ const Home = () => {
     <>
       <div className="container home-container">
         <h2>Click on a link to go to that page</h2>
-        <Link to="/degree-subject">My Degree Subject</Link>
-        <Link to="useCases">Amazing use cases of Web Development</Link>
-        <Link to="technical-challenges">
+        <Link to="/degree-subject" className="goto-page-links">
+          My Degree Subject
+        </Link>
+        <Link to="useCases" className="goto-page-links">
+          Amazing use cases of Web Development
+        </Link>
+        <Link to="technical-challenges" className="goto-page-links">
           Technical Challenges of Web Development
         </Link>
         <Link></Link>
